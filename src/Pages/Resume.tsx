b8ec9d1ab1dc8d0dbcaef66f832462aaa3e6d850
pramodjangam <pramod.jangam@deserve.com>
2021-05-29 17:52:28 +0530

    Added google docs resume

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+const GoogleDocsViewer = require('react-google-docs-viewer');
 
 const Resume = () => {
-  return <>This is some Resume page</>;
+  const style = {
+    width: '95%',
+    height: '80vh',
+    margin: '20 0 0 0',
+  };
+  return (
+    <div>
+      <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vROZATH6LmiBw0HomEu0qR0nqKl4lQTHmG4O06tLutBT6qljqId4SamDUDUYXkzVg/pub?embedded=true"
+        style={style}
+      ></iframe>
+    </div>
+  );
 };
 
 export { Resume };
