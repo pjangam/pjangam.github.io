57bc127b040bcbfa89bedf3832fe648c0fa700ea
Pramod Jangam <pjangam2015@gmail.com>
2023-04-30 13:41:03 +0530

    - updated google doc link to google doc instead of docx for google app script support - added download button

--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 const Resume = () => {
+  console.log(document.documentElement.clientWidth);
+  const screenWidth = document.documentElement.clientWidth;
+  const margin = (screenWidth - 900) / 2;
+  let divstyle = { 'marginLeft': '0px', 'marginRight': '0px' }
+
+  if (margin > 0) {
+    divstyle = { 'marginLeft': `${margin}px`, 'marginRight': `${margin}px` }
+  }
+
+  function download() {
+    var link = document.createElement('a');
+    link.href = "https://drive.google.com/file/d/1nLrseDI2qSG6TEN-pjSmgboOdRw-fFZA/view?usp=share_link";
+    link.download = 'true';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+  }
   const style = {
     width: '95%',
     height: '80vh',
     margin: '0 0 0 20px',
+    'text-align': 'center',
+    'justify-content': 'center'
   };
   return (
-    <div>
+    <div style={divstyle}>
+      <Button outline color="success" onClick={download} style={{ "float": "right", "marginRight": "50px" }}>Download <i className="fa fa-download"></i></Button><br /><br /><br />
       <iframe
         title="GoogleDocsResume"
-        src="https://docs.google.com/document/d/e/2PACX-1vROZATH6LmiBw0HomEu0qR0nqKl4lQTHmG4O06tLutBT6qljqId4SamDUDUYXkzVg/pub?embedded=true"
+        src="https://docs.google.com/document/d/e/2PACX-1vQdtcDDTw6lrN6UVagqXohSvWRCLvIe510v2sKGZbopXsoNbWHurWItPX-xqxeKPbZxapkqQdXdt7_f/pub?embedded=true"
         style={style}
       ></iframe>
     </div>
